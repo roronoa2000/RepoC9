82615bdb5f3bc1dac31f4d21943396735e007d0c
Aurelio Hernandez Briones <saoa_taoem@hotmail.com>
2015-04-16 22:04:57 -0500

    resp prueba 1

---
+++ app/Model/Academica.php
@@ -0,0 +1,75 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * Academica Model
+ *
+ * @property Alumno $Alumno
+ */
+class Academica extends AppModel {
+
+/**
+ * Display field
+ *
+ * @var string
+ */
+	public $displayField = 'matricula';
+
+/**
+ * Validation rules
+ *
+ * @var array
+ */
+	public $validate = array(
+		'matricula' => array(
+			'unique' => array(
+				'rule' => array('isUnique',
+				'message' => 'La matricula ya tiene un credito asignado',
+				)
+				),
+
+			'numeric' => array(
+				'rule' => array('numeric'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+			'notEmpty' => array(
+				'rule' => array('notEmpty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'porcentaje' => array(
+			'numeric' => array(
+				'rule' => array('numeric'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+	);
+
+	//The Associations below have been created with all possible keys, those that are not needed can be removed
+
+/**
+ * belongsTo associations
+ *
+ * @var array
+ */
+	public $belongsTo = array(
+		'Alumno' => array(
+			'className' => 'Alumno',
+			'foreignKey' => 'alumno_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		)
+	);
+}
